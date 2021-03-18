c8114b3b2e22b117434328a3106128888ee4610a
gui2one <guillaume.rouault.fx@gmail.com>
2021-03-18 16:10:08 +0100

    moved Renderer init a very beginning of Appliaction constructor

--- src/Orbitons/core/Application.cpp
+++ src/Orbitons/core/Application.cpp
@@ -1,10 +1,13 @@
 #include "Application.h"
-namespace Orbitons{
+namespace Orbitons
+{
 
-    Application* Application::s_instance = nullptr;
-    Application * Application::getInstance(){
+    Application *Application::s_instance = nullptr;
+    Application *Application::getInstance()
+    {
 
-        if(s_instance == nullptr){
+        if (s_instance == nullptr)
+        {
 
             s_instance = new Application();
         }
@@ -12,49 +15,42 @@ namespace Orbitons{
         return s_instance;
     }
 
-    Application::Application(){
-
-        
-
-        m_window = std::make_unique<Window>();
-
-        m_window->setEventCallback(BIND_EVENT_FUNCTION(Application::onEvent));
-        m_window->m_ui.setEventCallback(BIND_EVENT_FUNCTION(Application::onEvent));
-        m_window->m_controls.setEventCallback(BIND_EVENT_FUNCTION(Application::onEvent));
+    Application::Application()
+    {
 
         Renderer::setAPI(RenderAPI::OpenGL);
         Renderer::init();
 
-
+        m_window = std::make_unique<Window>();
+        m_window->setEventCallback(BIND_EVENT_FUNCTION(Application::onEvent));
+        m_window->m_ui.setEventCallback(BIND_EVENT_FUNCTION(Application::onEvent));
+        m_window->m_controls.setEventCallback(BIND_EVENT_FUNCTION(Application::onEvent));
     }
 
-
-
-    void Application::run(){
-        while(!m_window->shouldClose()){
+    void Application::run()
+    {
+        while (!m_window->shouldClose())
+        {
 
             // m_eventQueue->process();
-            
+
             float delta_time = m_timer.getDeltaTime();
             m_timer.update();
-            
 
             float angle = m_timer.getElapsedTime();
 
-            glm::vec3 up_vector = glm::vec3(0.0f, 1.0f, 0.0f);        
-
+            glm::vec3 up_vector = glm::vec3(0.0f, 1.0f, 0.0f);
 
             m_window->refresh(m_timer);
-
-
         }
 
         m_window->m_ui.close();
-    
+
         glfwTerminate();
     }
 
-    void Application::onEvent(Event& e){
+    void Application::onEvent(Event &e)
+    {
 
         m_window->m_ui.onEvent(e);
         m_window->m_controls.onEvent(e);
@@ -62,6 +58,5 @@ namespace Orbitons{
 
         // printf("[Application Event] %s\n", e.GetName());
     }
-
 
 }
