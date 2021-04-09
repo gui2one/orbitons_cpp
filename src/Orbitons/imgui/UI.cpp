be89a5ba954cd099e2cc388ca19fbc4ede946db8
gui2one <guillaume.rouault.fx@gmail.com>
2021-04-09 13:07:13 +0200

    clean up

--- src/Orbitons/imgui/UI.cpp
+++ src/Orbitons/imgui/UI.cpp
@@ -46,9 +46,11 @@ namespace Orbitons
         ImVec4 *colors = ImGui::GetStyle().Colors;
         // small scope to tidy a little
         {
+
+            ImVec4 *colors = ImGui::GetStyle().Colors;
             colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
             colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-            colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
+            colors[ImGuiCol_WindowBg] = ImVec4(0.12f, 0.12f, 0.12f, 0.94f);
             colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
             colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
             colors[ImGuiCol_Border] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
@@ -56,7 +58,7 @@ namespace Orbitons
             colors[ImGuiCol_FrameBg] = ImVec4(0.16f, 0.29f, 0.48f, 0.54f);
             colors[ImGuiCol_FrameBgHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
             colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
-            colors[ImGuiCol_TitleBg] = ImVec4(0.16f, 0.19f, 0.26f, 1.00f);
+            colors[ImGuiCol_TitleBg] = ImVec4(0.04f, 0.04f, 0.04f, 1.00f);
             colors[ImGuiCol_TitleBgActive] = ImVec4(0.16f, 0.29f, 0.48f, 1.00f);
             colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
             colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
@@ -70,10 +72,9 @@ namespace Orbitons
             colors[ImGuiCol_Button] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
             colors[ImGuiCol_ButtonHovered] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
             colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
-            // colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
-            colors[ImGuiCol_Header] = ImVec4(0.76f, 0.35f, 0.22f, 0.8f);
-            colors[ImGuiCol_HeaderHovered] = ImVec4(0.76f, 0.35f, 0.22f, 0.3f);
-            colors[ImGuiCol_HeaderActive] = ImVec4(0.76f, 0.35f, 0.22f, 1.0f);
+            colors[ImGuiCol_Header] = ImVec4(1.00f, 0.73f, 0.34f, 0.52f);
+            colors[ImGuiCol_HeaderHovered] = ImVec4(0.95f, 0.65f, 0.20f, 0.82f);
+            colors[ImGuiCol_HeaderActive] = ImVec4(0.96f, 0.53f, 0.21f, 0.94f);
             colors[ImGuiCol_Separator] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
             colors[ImGuiCol_SeparatorHovered] = ImVec4(0.10f, 0.40f, 0.75f, 0.78f);
             colors[ImGuiCol_SeparatorActive] = ImVec4(0.10f, 0.40f, 0.75f, 1.00f);
@@ -91,11 +92,11 @@ namespace Orbitons
             colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
             colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
             colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
-            // colors[ImGuiCol_TableHeaderBg]          = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
-            // colors[ImGuiCol_TableBorderStrong]      = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
-            // colors[ImGuiCol_TableBorderLight]       = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
-            // colors[ImGuiCol_TableRowBg]             = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-            // colors[ImGuiCol_TableRowBgAlt]          = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+            colors[ImGuiCol_TableHeaderBg] = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
+            colors[ImGuiCol_TableBorderStrong] = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
+            colors[ImGuiCol_TableBorderLight] = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
+            colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+            colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
             colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
             colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
             colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
@@ -337,11 +338,11 @@ namespace Orbitons
         resourceLibraryPanel();
         EntityComponentsPanel();
 
-        // if (showDemoWindow)
-        // {
+        if (showDemoWindow)
+        {
 
-        //     ImGui::ShowDemoWindow(&showDemoWindow);
-        // }
+            ImGui::ShowDemoWindow(&showDemoWindow);
+        }
 
         // Rendering
         ImGui::Render();
